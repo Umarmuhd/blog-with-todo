297795d0be8708c59d908c40a34b084a06ba374b
umarmuhd <umarfarouqft@gmail.com>
2022-03-21 23:49:36 +0100

    changed loading spinner color to black

--- src/pages/Blog/SinglePost.jsx
+++ src/pages/Blog/SinglePost.jsx
@@ -15,12 +15,12 @@ const SpinIcon = () => (
       cx="12"
       cy="12"
       r="10"
-      stroke="#006156"
+      stroke="#000000"
       strokeWidth="4"
     ></circle>
     <path
       className="opacity-75"
-      fill="#006156"
+      fill="#000000"
       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
     ></path>
   </svg>
